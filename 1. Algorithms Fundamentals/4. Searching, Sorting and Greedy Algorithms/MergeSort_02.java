23a1357441ec6fe9a46b978a15c3c986d53827ef
Mario Emilov <88974458+MarioEmilov@users.noreply.github.com>
2021-12-25 13:37:48 +0200

    Add files via upload

---
+++ 1. Algorithms Fundamentals/4. Searching, Sorting and Greedy Algorithms/MergeSort_02.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MergeSort_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        sort(array);
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int number : array) {
+            builder.append(number).append(" ");
+        }
+        System.out.println(builder.toString());
+    }
+
+    private static void sort(int[] array) {
+        mergeSort(array, 0, array.length - 1);
+    }
+
+    private static void mergeSort(int[] array, int begin, int end) {
+        if (begin >= end) {
+            return;
+        }
+
+        int halfIndex = (begin + end) / 2;
+
+        mergeSort(array, begin, halfIndex);
+        mergeSort(array, halfIndex + 1, end);
+
+        merge(array, begin, halfIndex, end);
+    }
+
+    private static void merge(int[] array, int begin, int mid, int end) {
+        if (mid < 0 || mid >= array.length || array[mid] < array[mid + 1]) {
+            return;
+        }
+
+        int[] originalData = new int[array.length];
+
+        for (int i = begin; i <= end; i++) {
+            originalData[i] = array[i];
+        }
+
+        int leftIndex = begin;
+        int rightIndex = mid + 1;
+
+        for (int i = begin; i <= end; i++) {
+            if (leftIndex > mid) {
+                array[i] = originalData[rightIndex++];
+            } else if (rightIndex > end) {
+                array[i] = originalData[leftIndex++];
+            } else if (originalData[leftIndex] <= originalData[rightIndex]) {
+                array[i] = originalData[leftIndex++];
+            } else if (originalData[leftIndex] > originalData[rightIndex]) {
+                array[i] = originalData[rightIndex++];
+            }
+        }
+    }
+}
---
+++ 1. Algorithms Fundamentals/5. Graph Theory, Traversal and Shortest Paths Lab/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Deque<Integer>> getConnectedComponents(List<List<Integer>> graph) {
+        throw new AssertionError("Not Implemented");
+    }
+
+    public static Collection<String> topSort(Map<String, List<String>> graph) {
+        throw new AssertionError("Not Implemented");
+    }
+}
